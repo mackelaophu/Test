15c08aeeba5ab2c864b4e5a27f7d7ef071fc9642
dangph3 <dangph3@vpbank.com>
2024-03-20 21:25:47 +0700

    function expresion

--- test.js
+++ test.js
@@ -7,21 +7,6 @@
 //     {name: 'Alaska', capital: 'Juneau'},
 //     {name: 'Arizona', capital: 'Phoenix'}
 // ]
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -30,33 +15,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
 };
 // for (const state of states) {
 //     console.log(state.capital);
@@ -79,22 +37,18 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 //         return shape.width * shape.width;
 //     }
 // }
-var Square = /** @class */ (function () {
-    function Square(width) {
+class Square {
+    constructor(width) {
         this.width = width;
     }
-    return Square;
-}());
-var Rectangle = /** @class */ (function (_super) {
-    __extends(Rectangle, _super);
-    function Rectangle(width, height) {
-        var _this = _super.call(this, width) || this;
-        _this.width = width;
-        _this.height = height;
-        return _this;
+}
+class Rectangle extends Square {
+    constructor(width, height) {
+        super(width);
+        this.width = width;
+        this.height = height;
     }
-    return Rectangle;
-}(Square));
+}
 function cal(shape) {
     if (shape instanceof Rectangle) {
         return shape.width * shape.height;
@@ -115,7 +69,7 @@ function setLightSwitch(value) {
             turnLightOff();
             break;
         default:
-            console.log("I'm afraid I can't do that.");
+            console.log(`I'm afraid I can't do that.`);
     }
 }
 function turnLightOn() {
@@ -125,23 +79,71 @@ function turnLightOff() {
     console.log("turnLightOff");
 }
 function setLight() {
-    return __awaiter(this, void 0, void 0, function () {
-        var response, result;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0: return [4 /*yield*/, fetch("/light")];
-                case 1:
-                    response = _a.sent();
-                    return [4 /*yield*/, response.json()];
-                case 2:
-                    result = _a.sent();
-                    setLightSwitch(result.lightSwitchValue);
-                    return [2 /*return*/];
-            }
-        });
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield fetch("/light");
+        const result = yield response.json();
+        setLightSwitch(result.lightSwitchValue);
     });
 }
-function add(a, b) {
-    return a + b;
+function calculateLen(v) {
+    return v.x + v.y;
+}
+const v = { name: "a", x: 3, y: 4 };
+calculateLen(v);
+// const vec3D = {x : 3, y: 4, z:5, address: 'a'}
+// function calculateLengthL1(v: Vector3D) {
+//   let length = 0;
+//   for (const axis of Object.keys(v)) {
+//     const coord = v[axis];
+//     // ~~~~~~~ Element implicitly has an 'any' type because ...
+//     //         'string' can't be used to index type 'Vector3D'
+//     length += Math.abs(coord);
+//   }
+//   return length;
+// }
+function calculateAge(birthDate) {
+    // ...
+    return 1;
+}
+let birthDate = "1990-01-19";
+calculateAge(birthDate); // OK
+let blah = 1;
+function logMessage(msg) {
+    if (msg) {
+        msg;
+        console.log(msg);
+    }
+}
+const a = "A";
+const ab = Math.random() < 0.5 ? "A" : "B";
+const ab12 = ab; // OK, {"A", "B"} is a subset of {"A", "B", 12}
+const ps = {
+    name: "abc",
+    birth: new Date("1912/06/23"),
+    death: new Date("1912/06/23"),
+};
+function getKey(val, key) {
+}
+getKey({}, "x");
+getKey(23, ab);
+function sortBy(list, key) {
+    const blah = list.sort((a, b) => a[key] > b[key] ? 1 : (a[key] < b[key] ? -1 : 0));
+    return blah;
+}
+let list = [
+    { name: 'nguyen van a', role: 'cvcc', age: 29 },
+    { name: 'nguyen van c', role: 'cvcd', age: 30 },
+    { name: 'nguyen van b', role: 'cvac', age: 31 },
+];
+printVal(sortBy(list, 'age'));
+console.log('=================');
+printVal(sortBy(list, 'name'));
+console.log('=================');
+printVal(sortBy(list, 'role'));
+console.log('=================');
+function printVal(list) {
+    list.forEach(value => {
+        console.log(`name: ${value.name} age: ${value.age} role: ${value.role}`);
+    });
 }
 //# sourceMappingURL=test.js.map
--- test.ts
+++ test.ts
@@ -89,3 +89,133 @@ async function setLight() {
 //  function add(a,b) {
 //   return a + b;
 //  }
+
+interface Vector2D {
+  x: number;
+  y: number;
+}
+
+function calculateLen(v: Vector2D) {
+  return v.x + v.y;
+}
+
+interface NameVector {
+  name: string;
+  x: number;
+  y: number;
+}
+
+const v: NameVector = { name: "a", x: 3, y: 4 };
+calculateLen(v);
+interface Vector3D {
+  x: number;
+  y: number;
+  z: number;
+}
+// const vec3D = {x : 3, y: 4, z:5, address: 'a'}
+// function calculateLengthL1(v: Vector3D) {
+//   let length = 0;
+
+//   for (const axis of Object.keys(v)) {
+//     const coord = v[axis];
+//     // ~~~~~~~ Element implicitly has an 'any' type because ...
+//     //         'string' can't be used to index type 'Vector3D'
+//     length += Math.abs(coord);
+//   }
+//   return length;
+// }
+function calculateAge(birthDate: Date): number {
+  // ...
+  return 1;
+}
+let birthDate: any = "1990-01-19";
+calculateAge(birthDate); // OK
+
+let blah = 1;
+
+function logMessage(msg: string | null) {
+  if (msg) {
+    msg;
+    console.log(msg);
+  }
+}
+
+type AB = "A" | "B";
+type AB12 = "A" | "B" | 12;
+const a: AB = "A";
+
+const ab: AB = Math.random() < 0.5 ? "A" : "B";
+const ab12: AB12 = ab; // OK, {"A", "B"} is a subset of {"A", "B", 12}
+// declare let twelve: AB12;
+// const back: AB = twelve;
+interface Person {
+  name: string;
+}
+
+interface LifeSpan {
+  birth: Date;
+  death?: Date;
+}
+
+type PersonSpan = Person & LifeSpan;
+
+const ps: PersonSpan = {
+  name: "abc",
+  birth: new Date("1912/06/23"),
+  death: new Date("1912/06/23"),
+};
+
+type K = keyof (Person | LifeSpan);
+type S = keyof AB12;
+interface PersonSpan2 extends Person {
+  age: string;
+}
+
+type PS2 = keyof PersonSpan2;
+
+function getKey<K extends string>(val: any, key: K) {}
+getKey({}, "x");
+getKey(23, ab);
+// getKey({},12);
+type blah = keyof PersonSpan;
+
+interface Staff {
+  name: string;
+  role: string;
+  age: number;
+}
+
+type StaffKeys = keyof Staff;
+function sortBy<T, K extends keyof T>(list: T[], key: K): T[] {
+  const blah = list.sort((a, b) =>
+    a[key] > b[key] ? 1 : a[key] < b[key] ? -1 : 0
+  );
+  return blah;
+}
+
+let list: Staff[] = [
+  { name: "nguyen van a", role: "cvcc", age: 29 },
+  { name: "nguyen van c", role: "cvcd", age: 30 },
+  { name: "nguyen van b", role: "cvac", age: 31 },
+];
+printVal(sortBy(list, "age"));
+console.log("=================");
+printVal(sortBy(list, "name"));
+console.log("=================");
+printVal(sortBy(list, "role"));
+console.log("=================");
+
+function printVal<T extends Staff>(list: T[]) {
+  list.forEach((value) => {
+    console.log(`name: ${value.name} age: ${value.age} role: ${value.role}`);
+  });
+}
+
+const funcA = function (a: number): number {
+  return 0;
+};
+const funcB = (a: number): number => {
+  return 0;
+};
+type BlahFunc = (a: number) => number;
+declare function BLohFunc(a: number): number;
